f283811453fb0a005c59a03d5aabb04f7aec9e6e
Adipi Manojkumar <adipimanojkumar@gmail.com>
2024-08-07 00:57:13 +0530

    zoom

---
+++ app.js
@@ -0,0 +1,23 @@
+ //document.write("helloworld");
+ //alert("click ok to close");
+ //console.log("hellobabai");/*console.log("hello");*/
+
+//  let name="hello";
+//  console.log(name)
+// var someValue ="somedata";
+// // using let
+// let firstName = "Boby";
+// // using const
+// const address = " hyd";
+// console.log(someValue);
+// console.log(firstName);
+// console.log(address);
+// console.log("my anme is:"+firstName+address)
+
+// let website = "google"
+// let url = "http://"+ website +".com";
+// console.log(url);
+const number = 123;
+const secondnum = 2.1234;
+console.log(secondnum);
+console.log(number);
